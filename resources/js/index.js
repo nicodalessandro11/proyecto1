d9c0b4e6c37d62fd1f91540b5e97f66fc89b377e
nicodalessandro11 <nicodalessandro11@gmail.com>
2023-06-01 20:59:49 +0200

    owl carrousel

--- resources/js/index.js
+++ resources/js/index.js
@@ -16,3 +16,41 @@ function createProjectElement(project) {
 const projectRenderer = new ProjectRenderer();
 
 projectRenderer.fetchProjects(projectsParentElement, createProjectElement);
+
+
+/* MENU */
+const btnHamburger = document.querySelector('#btnHamburger');
+const btnCross = document.querySelector('#btnCross');
+const menu = document.querySelector('.menu');
+
+btnHamburger.addEventListener('click',(e) => {
+  e.target.style.opacity = '0';
+  e.target.style.zIndex = '0';
+  btnCross.style.opacity = '1';
+  btnCross.style.zIndex = '1';
+  menu.style.height = '210px';
+})
+
+btnCross.addEventListener('click',(e) => {
+  e.target.style.opacity = '0';
+  e.target.style.zIndex = '0';
+  btnHamburger.style.opacity = '1';
+  btnHamburger.style.zIndex = '1';
+  menu.style.height = '0';
+})
+
+/* OWL CAROUSEL */
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    animateIn: 'linear',
+    animateOut: 'linear',
+    smartSpeed: 2000,
+    autoplayTimeout: 2000,
+    autoplayHoverPause:false
+  });
+});
